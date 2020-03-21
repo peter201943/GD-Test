151a2438e3144b162d84ecba0d02820551f98389
pjm349@drexel.edu <pjm349@drexel.edu>
2020-03-20 22:16:14 -0400

    More Learning

---
+++ AnotherBehavior.gd
@@ -0,0 +1,9 @@
+extends Node
+# How many scripts can a single thing have?
+
+func _ready():
+	print("YET ANON!")
+
+# Now my question is, how many behaviors can we add in?
+# How do we avoid getting cluttered scripts?
+
--- Box.gd
+++ Box.gd
@@ -1,33 +1,66 @@
+
+
+
+
+### DECLARATIONS ###
+class_name SimpleTest
 extends KinematicBody
-
-
-# CONSTANTS
-const move_velocity := Vector3(1,1,1)		# Allows moving of box around
-const down_force := -0.8
+# Just a simple script to get familiars with GDScript
 
 
 
-# ENGINE
+
+### REFERENCES ###
+# [Make Your First 2D Game with Godot: Player and Enemy (beginner tutorial part 1)](https://www.youtube.com/watch?v=Mc13Z2gboEk)
+# https://docs.godotengine.org/en/stable/getting_started/scripting/gdscript/gdscript_styleguide.html
+
+
+
+
+### VALUES ###
+const FORCE_DOWN := -0.8				# How fast we Fall
+var current_speed := Vector3(1,1,1)		# Current Speed
+export var max_speed := 0.5				# How fast we can move
+
+
+
+
+### BUILT-INS ###
 func _ready():
-	test_print()
+	_test_print()
+
 
 func _physics_process(delta: float) -> void:
-	move_box(delta)
+	_move_box(delta)
+	_check_speed()
 
 
 
 
-# MECHANICS
-func move_box(delta: float) -> void:
+### PRIVATE METHODS ###
+func _move_box(delta: float) -> void:
 	"""Tests the Slipping and Sliding of an Object"""
-	self.move_and_slide(move_velocity)
-	move_velocity.y += down_force * delta
+# warning-ignore:return_value_discarded
+	self.move_and_slide(current_speed)
+	current_speed.y += FORCE_DOWN * delta
+	current_speed.x -= FORCE_DOWN * delta
+	current_speed.z -= FORCE_DOWN * delta
 
 
-func test_print():
+func _test_print():
 	"""Tests the Printing of some stuff"""
 	print("Hello (print)")
 	print_debug("Hello (debug)")
 	printerr("Hello (err)")
 	printraw("Hello (raw)")
+
+	
+func _check_speed():
+	"""Resets speed if over a certain value"""
+	if ((current_speed.x > max_speed) or (current_speed.y > max_speed) or (current_speed.z > max_speed)):
+		current_speed.x = 0
+		current_speed.y = 0
+		current_speed.z = 0
+
+
 
